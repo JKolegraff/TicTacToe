2da2b8fb671c96cc760ecd231b48b982fe197add
JK <JKolegraff@outlook.com>
2025-07-01 10:43:47 -0400

    updating subscribe

--- js/pages/TicTacToe/game-logic.js
+++ js/pages/TicTacToe/game-logic.js
@@ -8,9 +8,11 @@ export async function updateGame(data) {
     renderBoardToScreen();
 
     let thisPlayer = getPlayer();
+    console.log('Current player:', thisPlayer);
 
     thisPlayer === data.turn ? enableBoard() : disableBoard();
-    
+
     setInventory(data.player_state, thisPlayer);
     renderInventoryToScreen();
+    console.log('Game updated:', data);
 }
